ce77cad038dd614d94b041dd8b5dc9fd5933c26d
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-05-24 15:48:35 +0300

    Создает компонент App

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+// React
+import React from "react";
+
+// Components
+import {WelcomeScreen} from "../welcome-screen/welcome-screen";
+
+// Component App
+export const App = () => {
+  return <WelcomeScreen />;
+};

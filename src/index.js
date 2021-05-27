42b2fcc45eda4c94babc6da2ca8ff3e9d9c9acb2
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-05-27 18:21:00 +0300

    Подключает в приложение react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 // React
-// React
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Router
+import {BrowserRouter} from "react-router-dom";
 
 // Components
 import {App} from "./components/app/app";
 
 // Application initialization
 const init = () => {
-  ReactDOM.render(<App />, document.getElementById(`root`));
+  ReactDOM.render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      document.getElementById(`root`)
+  );
 };
 
 init();

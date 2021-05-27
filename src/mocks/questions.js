5bfda6a840dccf2eaa8bcfbd05a77a13611f6df1
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-05-27 18:37:40 +0300

    Создает моковые данные с вопросами

---
+++ src/mocks/questions.js
@@ -0,0 +1,218 @@
+const MUSIC_URL = `https://cdn.drivemusic.me/dl/online/f4uMEZC2zCPDXtun5ieS1Q/1620944735/download_music/2019/03/paul-mauriat-adagio-albinoni.mp3`;
+
+export const questions = [
+  {
+    type: `artist`,
+    song: {
+      artist: `Antonio Vivaldi`,
+      src: MUSIC_URL,
+    },
+    answers: [
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Antonio Vivaldi`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Hildegard von Bingen`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Johann Sebastian Bach`,
+      },
+    ],
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `Antonio Vivaldi`,
+      src: MUSIC_URL,
+    },
+    answers: [
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Johann Sebastian Bach`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Antonio Vivaldi`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Wolfgang Amadeus Mozart`,
+      },
+    ],
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `Antonio Vivaldi`,
+      src: MUSIC_URL,
+    },
+    answers: [
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Ludwig van Beethoven`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Wolfgang Amadeus Mozart`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Antonio Vivaldi`,
+      },
+    ],
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `Antonio Vivaldi`,
+      src: MUSIC_URL,
+    },
+    answers: [
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Hildegard von Bingen`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Antonio Vivaldi`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Johann Sebastian Bach`,
+      },
+    ],
+  },
+  {
+    type: `genre`,
+    genre: `reggae`,
+    answers: [
+      {
+        src: MUSIC_URL,
+        genre: `alternative`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `reggae`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `electronic`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `reggae`,
+      },
+    ],
+  },
+  {
+    type: `genre`,
+    genre: `electronic`,
+    answers: [
+      {
+        src: MUSIC_URL,
+        genre: `reggae`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `electronic`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `alternative`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `alternative`,
+      },
+    ],
+  },
+  {
+    type: `genre`,
+    genre: `reggae`,
+    answers: [
+      {
+        src: MUSIC_URL,
+        genre: `reggae`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `electronic`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `electronic`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `country`,
+      },
+    ],
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `Antonio Vivaldi`,
+      src: MUSIC_URL,
+    },
+    answers: [
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Johann Sebastian Bach`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Hildegard von Bingen`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Antonio Vivaldi`,
+      },
+    ],
+  },
+  {
+    type: `genre`,
+    genre: `electronic`,
+    answers: [
+      {
+        src: MUSIC_URL,
+        genre: `electronic`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `country`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `electronic`,
+      },
+      {
+        src: MUSIC_URL,
+        genre: `electronic`,
+      },
+    ],
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `Antonio Vivaldi`,
+      src: MUSIC_URL,
+    },
+    answers: [
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Wolfgang Amadeus Mozart`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Johann Sebastian Bach`,
+      },
+      {
+        picture: `https://placehold.co/134x134`,
+        artist: `Antonio Vivaldi`,
+      },
+    ],
+  },
+];

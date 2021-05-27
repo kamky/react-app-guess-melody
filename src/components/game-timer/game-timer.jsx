ef15f56abd4b64fe70f70d7d14b027e47797a3b7
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-05-27 17:34:34 +0300

    Создает компонент GameTimer

---
+++ src/components/game-timer/game-timer.jsx
@@ -0,0 +1,13 @@
+// React
+import React from "react";
+
+// Component GameTimer
+export const GameTimer = () => {
+  return (
+    <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
+      <span className="timer__mins">05</span>
+      <span className="timer__dots">:</span>
+      <span className="timer__secs">00</span>
+    </div>
+  );
+};

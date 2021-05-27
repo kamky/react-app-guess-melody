546a86d7bb2c023aafaaa4461e73765051b77e85
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-05-27 17:36:19 +0300

    Создает компонент GameMistakes

---
+++ src/components/game-mistakes/game-mistakes.jsx
@@ -0,0 +1,13 @@
+// React
+import React from "react";
+
+// Component GameMistakes
+export const GameMistakes = () => {
+  return (
+    <div className="game__mistakes">
+      <div className="wrong"></div>
+      <div className="wrong"></div>
+      <div className="wrong"></div>
+    </div>
+  );
+};

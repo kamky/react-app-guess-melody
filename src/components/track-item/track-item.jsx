7c1b0f323096b6f7d5884b9405a77ae434d82a2b
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-05-29 21:29:27 +0300

    Определяет пропсы для компонента TrackItem

--- src/components/track-item/track-item.jsx
+++ src/components/track-item/track-item.jsx
@@ -1,11 +1,19 @@
 // React
 import React from "react";
+import PropTypes from "prop-types";
 
 // Components
 import {AudioPlayer} from "../audio-player/audio-player";
 
 // Component TrackItem
-export const TrackItem = () => {
+export const TrackItem = (props) => {
+  const {index, userAnswer, onInputChange} = props;
+
+  const handleChange = (evt) => {
+    const value = evt.target.checked;
+    onInputChange(index, value);
+  };
+
   return (
     <div className="track">
       <AudioPlayer />
@@ -14,13 +22,21 @@ export const TrackItem = () => {
           className="game__input visually-hidden"
           type="checkbox"
           name="answer"
-          value="answer-1"
-          id="answer-2"
+          value={`answer-${index}`}
+          id={`answer-${index}`}
+          checked={userAnswer}
+          onChange={handleChange}
         />
-        <label className="game__check" htmlFor="answer-2">
+        <label className="game__check" htmlFor={`answer-${index}`}>
           Отметить
         </label>
       </div>
     </div>
   );
+};
+
+TrackItem.propTypes = {
+  index: PropTypes.number.isRequired,
+  userAnswer: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
 };

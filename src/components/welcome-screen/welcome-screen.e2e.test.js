1e070d606af08bb2799bac9efc904553f84bc18e
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-06-02 20:03:56 +0300

    Обновляет e2e тест у компонента WelcomeScreen

--- src/components/welcome-screen/welcome-screen.e2e.test.js
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -1,7 +1,10 @@
 // Modules
+import "jsdom-global/register";
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {mount} from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import {createMemoryHistory} from "history";
+import {Router} from "react-router-dom";
 
 // Components
 import {WelcomeScreen} from "./welcome-screen";
@@ -13,7 +16,13 @@ Enzyme.configure({adapter: new Adapter()});
 describe(`e2e test for component <WelcomeScreen />`, () => {
   const clickHandler = jest.fn();
   const defaultProps = {onStartGameButtonClick: clickHandler};
-  const wrapper = shallow(<WelcomeScreen {...defaultProps} />);
+
+  const history = createMemoryHistory();
+  const wrapper = mount(
+      <Router history={history}>
+        <WelcomeScreen {...defaultProps} />
+      </Router>
+  );
 
   test(`should be called callback when the button is pressed`, () => {
     expect(wrapper.find(`button.welcome__button`).length).toBe(1);

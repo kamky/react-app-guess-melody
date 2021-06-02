805ebce49a067738fee78dc84dd0593c3886b967
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-06-02 21:26:24 +0300

    Определяет пропсы для компонента AudioPlayer

--- src/components/audio-player/audio-player.jsx
+++ src/components/audio-player/audio-player.jsx
@@ -1,17 +1,32 @@
 // React
 import React from "react";
+import PropTypes from "prop-types";
 
 // Component AudioPlayer
-export const AudioPlayer = () => {
+export const AudioPlayer = (props) => {
+  const {isPlaying, isLoading, onToggleAudioButtonClick, children} = props;
+
   return (
     <>
       <button
-        className="track__button track__button--play"
+        className={`track__button track__button--${
+          isPlaying ? `pause` : `play`
+        }`}
         type="button"
+        disabled={isLoading}
+        onClick={onToggleAudioButtonClick}
       ></button>
-      <div className="track__status">
-        <audio />
-      </div>
+      <div className="track__status">{children}</div>
     </>
   );
+};
+
+AudioPlayer.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  isPlaying: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  onToggleAudioButtonClick: PropTypes.func.isRequired,
 };

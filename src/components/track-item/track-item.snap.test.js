17feb3fc30e617f6814cb62616930d64b0322ccb
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-06-04 10:55:34 +0300

    Обновляет снимок для компонента TrackItem

--- src/components/track-item/track-item.snap.test.js
+++ src/components/track-item/track-item.snap.test.js
@@ -6,7 +6,16 @@ import renderer from "react-test-renderer";
 import {TrackItem} from "./track-item";
 
 describe(`Snapshot test for component <TrackItem />`, () => {
-  const defaultProps = {index: 0, userAnswer: false, onInputChange: jest.fn()};
+  const defaultProps = {
+    index: 0,
+    userAnswer: false,
+    onInputChange: jest.fn(),
+    renderAudioPlayer: jest.fn(),
+    answer: {
+      src: `src-url`,
+      genre: `genre-name`,
+    }
+  };
   const wrapper = renderer.create(<TrackItem {...defaultProps} />);
   test(`should component <TrackItem /> renders correctly`, () => {
     expect(wrapper).toMatchSnapshot();
